c700c88eb58088f948389e05aa05680ee140296e
Sachindra Maharjan <sachindra.maharjan4@gmail.com>
2021-05-08 14:50:37 -0400

    Updated fixture event.

--- database/fixture_event.go
+++ database/fixture_event.go
@@ -4,14 +4,19 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+
+	"cloud.google.com/go/firestore"
 )
 
 type FixtureEventService dbservice
 
+type FixtureEvents struct {
+	HomeTeam []FixtureEvent `firestore:"home_team,omitempty"`
+	AwayTeam []FixtureEvent `firestore:"away_team,omitempty"`
+}
+
 //FixtureEvent contains the fixture event infromation
 type FixtureEvent struct {
-	LeagueID       int    `firestore:"league_id"`
-	FixtureID      int    `firestore:"fixture_id"`
 	Elapsed        int    `firestore:"elapsed"`
 	ElapsedPlus    int    `firestore:"elapsed_plus,omitempty"`
 	TeamID         int    `firestore:"team_id"`
@@ -34,59 +39,82 @@ func (s *FixtureEventService) Add(ctx context.Context, leagueName string, record
 	fmt.Printf("Adding %d new fixture event data to firestore \n", len(records))
 
 	batch := s.client.fs.Batch()
-
-	eventMap := map[int]Events{}
-	var events Events
-	var assist string
-	var assistId int
+	var events FixtureEvents
+	var homeTeam map[int][]FixtureEvent
+	var awayTeam map[int][]FixtureEvent
+	var leagueId int
+	var fixtureId int
+	var currentFixture int
+	var homeTeamId int
+	var awayTeamId int
 
 	for _, r := range records {
-
-		if parseInt(r[8]) > 0 {
-			assistId = parseInt(r[8])
-		}
-		if len(r[9]) > 0 {
-			assist = r[9]
-		}
+		leagueId = parseInt(r[0])
+		fixtureId = parseInt(r[1])
 
 		f := FixtureEvent{}
-		f.LeagueID = parseInt(r[0])
-		f.FixtureID = parseInt(r[1])
 		f.Elapsed = parseInt(r[2])
 		f.ElapsedPlus = parseInt(r[3])
 		f.TeamID = parseInt(r[4])
 		f.TeamName = r[5]
 		f.PlayerID = parseInt(r[6])
 		f.Player = r[7]
-		f.AssistPlayerID = assistId
-		f.AssistedBy = assist
+		f.AssistPlayerID = parseInt(r[8])
+		f.AssistedBy = r[9]
 		f.Type = r[10]
 		f.Detail = r[11]
 		f.Comments = r[12]
 
-		if _, ok := eventMap[f.FixtureID]; ok {
-			events = eventMap[f.FixtureID]
-		} else {
-			events = Events{}
+		if currentFixture != fixtureId {
+			if currentFixture > 0 {
+				events.HomeTeam = homeTeam[homeTeamId]
+				events.AwayTeam = awayTeam[awayTeamId]
+				s.write(leagueName, leagueId, currentFixture, batch, events)
+			}
+			currentFixture = fixtureId
+			events = FixtureEvents{}
+			homeTeamId = 0
+			awayTeamId = 0
+			homeTeam = make(map[int][]FixtureEvent)
+			awayTeam = make(map[int][]FixtureEvent)
 		}
-		events.Event = append(events.Event, f)
-		eventMap[f.FixtureID] = events
+
+		if homeTeamId == 0 && awayTeamId == 0 {
+			isHomeTeam, err := s.isHomeTeam(ctx, leagueName, leagueId, fixtureId, f.TeamID)
+			if err != nil {
+				return err
+			}
+
+			if isHomeTeam {
+				homeTeamId = f.TeamID
+				homeTeam[f.TeamID] = []FixtureEvent{}
+			} else {
+				awayTeamId = f.TeamID
+				awayTeam[f.TeamID] = []FixtureEvent{}
+			}
+		}
+
+		if homeTeamId == 0 && awayTeamId != f.TeamID {
+			homeTeamId = f.TeamID
+			homeTeam[f.TeamID] = []FixtureEvent{}
+		}
+
+		if awayTeamId == 0 && homeTeamId != f.TeamID {
+			awayTeamId = f.TeamID
+			awayTeam[f.TeamID] = []FixtureEvent{}
+		}
+
+		if _, ok := homeTeam[f.TeamID]; ok {
+			homeTeam[f.TeamID] = append(homeTeam[f.TeamID], f)
+		} else {
+			awayTeam[f.TeamID] = append(awayTeam[f.TeamID], f)
+		}
 	}
 
-	for k, v := range eventMap {
-
-		leagueRef := s.client.fs.Collection("football-leagues").Doc(leagueName)
-		docRef := leagueRef.
-			Collection("leagues").
-			Doc("leagueId_" + strconv.Itoa(v.Event[0].LeagueID)).
-			Collection("fixtures").
-			Doc("fixtureId_" + strconv.Itoa(k)).
-			Collection("fixture_details").
-			Doc("events")
-
-		fmt.Printf("adding fixture event in %s\n", docRef.Path)
-
-		batch.Set(docRef, v)
+	if currentFixture > 0 {
+		events.HomeTeam = homeTeam[homeTeamId]
+		events.AwayTeam = awayTeam[awayTeamId]
+		s.write(leagueName, leagueId, currentFixture, batch, events)
 	}
 
 	_, err := batch.Commit(ctx)
@@ -96,4 +124,37 @@ func (s *FixtureEventService) Add(ctx context.Context, leagueName string, record
 	}
 
 	return nil
+}
+
+func (s *FixtureEventService) write(leagueName string, leagueId int,
+	fixtureId int, batch *firestore.WriteBatch, events FixtureEvents) {
+	leagueRef := s.client.fs.Collection("football-leagues").Doc(leagueName)
+	docRef := leagueRef.Collection("leagues").
+		Doc("leagueId_" + strconv.Itoa(leagueId)).
+		Collection("fixtures").
+		Doc("fixtureId_" + strconv.Itoa(fixtureId)).
+		Collection("fixture_details").
+		Doc("events")
+
+	fmt.Printf("adding fixture event in %s\n", docRef.Path)
+
+	batch.Set(docRef, events)
+}
+
+func (s *FixtureEventService) isHomeTeam(ctx context.Context, leagueName string,
+	leagueId int, fixtureId int, teamId int) (bool, error) {
+	leagueRef := s.client.fs.Collection("football-leagues").Doc(leagueName)
+	docRef := leagueRef.Collection("leagues").
+		Doc("leagueId_" + strconv.Itoa(leagueId)).
+		Collection("fixtures").
+		Doc("fixtureId_" + strconv.Itoa(fixtureId))
+	docSnap, err := docRef.Get(ctx)
+
+	if err != nil {
+		return false, err
+	}
+
+	var fixture Fixture
+	docSnap.DataTo(&fixture)
+	return fixture.HomeTeam.TeamID == teamId, nil
 }
